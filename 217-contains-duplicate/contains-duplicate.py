4d231bdbd8f126b4233c21ea755fe68afb597ce2
Osama Alnajar <osama.alnajar.26@gmail.com>
2026-01-19 02:29:46 +0000

    Time: 16 ms (70.21%) | Memory: 26.1 MB (22.96%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        
+        seen = set()
+
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
+            
+
+
+
+        
