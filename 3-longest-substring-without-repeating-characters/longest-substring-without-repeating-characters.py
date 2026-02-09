69f66e910d5ba78670a7e58a2ca2339fbf840042
Osama Alnajar <osama.alnajar.26@gmail.com>
2026-02-09 00:08:19 +0000

    Time: 20 ms (71.53%) | Memory: 12.6 MB (78.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,42 +1,49 @@
+"""
+hashtable --> char, index (last seen)
+
+firm my left pointer in 0
+
+my right is ohing to loop over each char in s:
+    if char has been seen before --> 
+    1. shift left piinter to char
+    2. update our dict (last seen index)
+
+    else:
+        hasnt been seen before -->
+        update max --> maxLen = max(maxLen, right - left + 1)
+
+
+    seen = {
+        a : 0,
+        b : 1,
+        c : 4,
+        d : 5
+    }
+
+"""
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         
-        '''
-        if len(s) < 2:
-            return len(s)
-
-        for x in range(len(s)):
-            seen = set()
-            count = 0
-            max_count = 0
-            print(s[x:])
-            for i in s[x:]:
-                if i not in seen:
-                    seen.add(i)
-                    count += 1
-                else:
-                    max_count = max(count, max_count)
-                    count = 1 
-                    seen = set()
-
-        return max(count, max_count)
-        '''
-
-        
-        seen = {}
-        max_len = 0
+        seen = {} #seen char --> char, last seen index
         left = 0
+        maxLen = 0
 
-        for right in range(len(s)): #abac
-            char = s[right]
-
-            if char in seen and seen[char] >= left:
-                left = seen[char] + 1
+        #right pointer
+        for index, value in enumerate(s): #abcccd
+            if value in seen and seen[value] >= left:
+                left = seen[value] + 1
             
-            seen[char] = right
-            max_len = max(max_len, right - left + 1)
+            seen[value] = index
+            maxLen = max(maxLen, index - left + 1) # maxLen = 3   left = 3
+        return maxLen 
+
         
-        return max_len
+            
+
+
+
+        
+        
 
 
 
